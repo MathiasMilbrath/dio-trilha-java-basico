d2edf4bd7894a5a9b53b875331a9037495a558f3
Mathias Milbrath <mathiasmilbrath2@gmail.com>
2024-05-15 23:24:35 -0300

    Resultado final

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -8,8 +8,16 @@ public class ContaTerminal {
 
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Digite a Agencia:\n");
+        System.out.println("Digite a Agencia: ");
         agencia = scanner.nextLine();
+        System.out.println("Digite o seu número: ");
+        numero = scanner.nextInt();
+        scanner.nextLine();
+        System.out.println("Digite seu nome: ");
+        nomeCliente = scanner.nextLine();
+        System.out.println("Digite seu saldo: ");
+        saldo = scanner.nextDouble();
+        System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta " + numero + " e seu saldo R$ " + saldo + " já está disponível para saque.");
 
     }
 }
